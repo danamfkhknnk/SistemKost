f829fd30952bf63cf716ae29e6b980a28b673d87
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2025-01-30 05:46:53 +0700

    try again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') != 'local') {
+            \URL::forceScheme('https');
+        }
+        
         View::composer('Home.Layout', function ($view) {
             $info = info::first(); // Ambil data info
             $poin = tentang::all(); // Ambil semua data tentang
