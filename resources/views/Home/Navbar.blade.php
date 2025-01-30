c1d3f36aee57d8fee8111bea7bdc805c1c947d42
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2025-01-30 10:53:43 +0700

    fix fix

--- resources/views/Home/Navbar.blade.php
+++ resources/views/Home/Navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 fixed top-0 right-0 w-full z-50">
   <div class="flex-1 flex justify-between items-center">
     <a href="#home">
-      <img src="{{ Storage::disk('public')->url('home/' . $info->logo) }}"  class="lg:w-[60px] w-[50px]">
+      <img src="{{ asset('home/' . $info->logo) }}"  class="lg:w-[60px] w-[50px]">
   </a>
 </div>
   <label for="menu-toggle" class="pointer-cursor lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
